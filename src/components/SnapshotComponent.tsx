1cee18a8a125a41dedd4b3feb4f919b5d8534709
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-10 09:25:23 +0900

    🚑 配布ファイルを追加

---
+++ src/components/SnapshotComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+  text: string
+}
+
+const SnapshotComponent: React.FC<Props> = ({ text }) => {
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default SnapshotComponent
