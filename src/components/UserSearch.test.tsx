2d87c2574f71579a41084651108bb9271ed8ec45
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-10 13:25:31 +0900

    ♻️ リファクタリング

--- src/components/UserSearch.test.tsx
+++ src/components/UserSearch.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import axios from 'axios'
 
@@ -12,29 +12,39 @@ describe('UserSearch', () => {
   beforeEach(() => mockAxios.get.mockReset())
 
   it('入力フィールドに値を入力し、検索ボンタンをクリックすると適切なAPIリクエストが発生する', async () => {
+    const userInfo = { id: 1, name: 'testUser1' }
+    mockAxios.get.mockResolvedValue({ data: userInfo })
+
     render(<UserSearch />)
     expect(screen.getByDisplayValue('')).toBeInTheDocument()
     const input = screen.getByRole('textbox')
-    await user.type(input, 'Test query')
-    expect(screen.getByDisplayValue('Test query')).toBeInTheDocument()
+    await user.type(input, userInfo.name)
+    expect(screen.getByDisplayValue(userInfo.name)).toBeInTheDocument()
 
-    mockAxios.get.mockResolvedValue({ data: { id: 1, name: 'testUser1' } })
     const button = screen.getByRole('button', { name: 'Search' })
     await user.click(button)
-    expect(mockAxios.get).toHaveBeenCalledWith('/api/users?query=Test query')
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      `/api/users?query=${userInfo.name}`,
+    )
   })
 
   it('APIから取得したユーザー情報が正しく画面に表示される', async () => {
+    const userInfo = { id: 1, name: 'testUser1' }
+    mockAxios.get.mockResolvedValue({ data: userInfo })
+
     render(<UserSearch />)
     expect(screen.getByDisplayValue('')).toBeInTheDocument()
     const input = screen.getByRole('textbox')
-    await user.type(input, 'Test query')
-    expect(screen.getByDisplayValue('Test query')).toBeInTheDocument()
+    await user.type(input, userInfo.name)
+    expect(screen.getByDisplayValue(userInfo.name)).toBeInTheDocument()
 
-    mockAxios.get.mockResolvedValue({ data: { id: 1, name: 'testUser1' } })
     const button = screen.getByRole('button', { name: 'Search' })
     await user.click(button)
-    expect(mockAxios.get).toHaveBeenCalledWith('/api/users?query=Test query')
-    expect(screen.getByText('testUser1')).toBeInTheDocument()
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      `/api/users?query=${userInfo.name}`,
+    )
+    await waitFor(() =>
+      expect(screen.getByText(userInfo.name)).toBeInTheDocument(),
+    )
   })
 })
